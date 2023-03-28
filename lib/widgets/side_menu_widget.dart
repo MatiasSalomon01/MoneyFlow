b9c13b230ce36ea08813f99cf3d3edb725924bf3
=Matias Salomon <matisalomond@gmail.com>
2023-03-28 10:53:30 -0400

    Side Menu ahora es un Column y no un ListView

--- lib/widgets/side_menu_widget.dart
+++ lib/widgets/side_menu_widget.dart
@@ -18,65 +18,67 @@ class _SideMenuState extends State<SideMenu> {
     return Drawer(
       child: SlideInLeft(
         duration: Duration(milliseconds: 350),
-        child: ListView(
-          children: [
-            ListTile(
-              title: const Text(
-                'Opciones',
-                style: TextStyle(fontSize: 20),
-              ),
-              trailing: IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  icon: Icon(Icons.close_sharp)),
-              //onTap: () => Navigator.pop(context),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 5),
-              child: ListTile(
-                leading: const Icon(Icons.home),
+        child: SafeArea(
+          child: Column(
+            children: [
+              ListTile(
                 title: const Text(
-                  'Pantalla Principal',
+                  'Opciones',
                   style: TextStyle(fontSize: 20),
                 ),
-                onTap: () => Navigator.pop(context),
+                trailing: IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(Icons.close_sharp)),
+                //onTap: () => Navigator.pop(context),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 5),
-              child: ListTile(
-                leading: const Icon(Icons.settings),
-                title:
-                    const Text('Configuración', style: TextStyle(fontSize: 20)),
-                onTap: () => Navigator.popAndPushNamed(context, 'settings'),
+              const SizedBox(
+                height: 15,
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 3),
-              child: ListTile(
-                leading: Preferences.isDarkMode
-                    ? Icon(Icons.dark_mode,
-                        color: Color.fromARGB(255, 71, 144, 204), size: 30)
-                    : Icon(Icons.light_mode,
-                        color: Color.fromARGB(255, 255, 196, 3), size: 30),
-                title: Preferences.isDarkMode
-                    ? Text('Modo Oscuro', style: TextStyle(fontSize: 20))
-                    : Text('Modo Claro', style: TextStyle(fontSize: 20)),
-                trailing: Switch.adaptive(
-                  activeColor: Colors.grey,
-                  value: Preferences.isDarkMode,
-                  onChanged: (value) {
-                    changeTheme(value, themeProvider);
-                    setState(() {});
-                  },
+              Padding(
+                padding: const EdgeInsets.only(left: 5),
+                child: ListTile(
+                  leading: const Icon(Icons.home),
+                  title: const Text(
+                    'Pantalla Principal',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  onTap: () => Navigator.pop(context),
                 ),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(left: 5),
+                child: ListTile(
+                  leading: const Icon(Icons.settings),
+                  title: const Text('Configuración',
+                      style: TextStyle(fontSize: 20)),
+                  onTap: () => Navigator.popAndPushNamed(context, 'settings'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 3),
+                child: ListTile(
+                  leading: Preferences.isDarkMode
+                      ? Icon(Icons.dark_mode,
+                          color: Color.fromARGB(255, 71, 144, 204), size: 30)
+                      : Icon(Icons.light_mode,
+                          color: Color.fromARGB(255, 255, 196, 3), size: 30),
+                  title: Preferences.isDarkMode
+                      ? Text('Modo Oscuro', style: TextStyle(fontSize: 20))
+                      : Text('Modo Claro', style: TextStyle(fontSize: 20)),
+                  trailing: Switch.adaptive(
+                    activeColor: Colors.grey,
+                    value: Preferences.isDarkMode,
+                    onChanged: (value) {
+                      changeTheme(value, themeProvider);
+                      setState(() {});
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
