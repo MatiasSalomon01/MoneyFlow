2f3549d12393ee8b6360e6642cb20e45810ef694
=Matias Salomon <matisalomond@gmail.com>
2023-03-28 13:49:22 -0400

    Limpieza de codigo

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
 
-class SearchScreen extends StatefulWidget {
+class SearchScreen extends StatelessWidget {
   SearchScreen({super.key});
 
   @override
-  State<SearchScreen> createState() => _SearchScreenState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Buscar'),
+      ),
+      body: Container(
+        margin: const EdgeInsets.only(top: 50),
+        child: Row(
+          children: const [
+            _SearchBar(),
+            _DropDownButton(),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class _SearchScreenState extends State<SearchScreen> {
+class _DropDownButton extends StatefulWidget {
+  const _DropDownButton({
+    super.key,
+  });
+
+  @override
+  State<_DropDownButton> createState() => _DropDownButtonState();
+}
+
+class _DropDownButtonState extends State<_DropDownButton> {
+  int _value = 1;
   List<DropdownMenuItem<int>> menuItems = [
     DropdownMenuItem(child: Text('Fecha'), value: 1),
     DropdownMenuItem(child: Text('Descripcion'), value: 2),
@@ -16,54 +41,50 @@ class _SearchScreenState extends State<SearchScreen> {
     DropdownMenuItem(child: Text('Egreso'), value: 5)
   ];
 
-  int _value = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(right: 20),
+      // color: Colors.red,
+      child: DropdownButton(
+        value: _value,
+        items: menuItems,
+        onChanged: (value) {
+          _value = value!;
+          setState(() {});
+        },
+      ),
+    );
+  }
+}
+
+class _SearchBar extends StatelessWidget {
+  const _SearchBar({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Buscar'),
-      ),
-      body: Container(
-        margin: EdgeInsets.only(top: 50),
-        child: Row(
-          children: [
-            Form(
-              child: Expanded(
-                child: Container(
-                  // color: Colors.green,
-                  padding: EdgeInsets.only(right: 30, left: 20),
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Buscar',
-                      prefixIcon: Icon(
-                        Icons.search,
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey, width: 2),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(50),
-                        ),
-                      ),
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
+    return Form(
+      child: Expanded(
+        child: Container(
+          // color: Colors.green,
+          padding: EdgeInsets.only(right: 30, left: 20),
+          child: TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Buscar',
+              prefixIcon: Icon(
+                Icons.search,
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey, width: 2),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(50),
                 ),
               ),
+              border: OutlineInputBorder(),
             ),
-            Container(
-              padding: EdgeInsets.only(right: 20),
-              // color: Colors.red,
-              child: DropdownButton(
-                value: _value,
-                items: menuItems,
-                onChanged: (value) {
-                  _value = value!;
-                  setState(() {});
-                },
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
