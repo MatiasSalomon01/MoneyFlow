9c79c832e6242933b8d4f1884ac76318bcb9671f
=Matias Salomon <matisalomond@gmail.com>
2023-03-29 11:05:02 -0400

    Refactorizacion de codigo

--- lib/widgets/side_menu_widget.dart
+++ lib/widgets/side_menu_widget.dart
@@ -23,78 +23,138 @@ class _SideMenuState extends State<SideMenu> {
         child: SafeArea(
           child: Column(
             children: [
-              ListTile(
-                title: const Text(
-                  'Opciones',
-                  style: TextStyle(fontSize: 20),
-                ),
-                trailing: IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: const Icon(Icons.close_sharp)),
-                //onTap: () => Navigator.pop(context),
-              ),
+              const _Options(),
               const SizedBox(
                 height: 15,
               ),
-              Padding(
-                padding: const EdgeInsets.only(left: 5),
-                child: ListTile(
-                  leading: const Icon(Icons.home),
-                  title: const Text(
-                    'Pantalla Principal',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  onTap: () => Navigator.pop(context),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 5),
-                child: ListTile(
-                  leading: const Icon(Icons.settings),
-                  title: const Text('Configuración',
-                      style: TextStyle(fontSize: 20)),
-                  onTap: () => Navigator.popAndPushNamed(context, 'settings'),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 5),
-                child: ListTile(
-                  leading: const Icon(Icons.search),
-                  title: const Text('Buscar', style: TextStyle(fontSize: 20)),
-                  onTap: () {
-                    searchScreenProvider.cards = [];
-                    searchScreenProvider.input = '';
-                    Navigator.popAndPushNamed(context, 'search');
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 3),
-                child: ListTile(
-                  leading: Preferences.isDarkMode
-                      ? const Icon(Icons.dark_mode,
-                          color: Color.fromARGB(255, 71, 144, 204), size: 30)
-                      : const Icon(Icons.light_mode,
-                          color: Color.fromARGB(255, 255, 196, 3), size: 30),
-                  title: Preferences.isDarkMode
-                      ? const Text('Modo Oscuro',
-                          style: TextStyle(fontSize: 20))
-                      : const Text('Modo Claro',
-                          style: TextStyle(fontSize: 20)),
-                  trailing: Switch.adaptive(
-                    activeColor: Colors.grey,
-                    value: Preferences.isDarkMode,
-                    onChanged: (value) {
-                      changeTheme(value, themeProvider);
-                      setState(() {});
-                    },
-                  ),
-                ),
-              ),
+              const _MainMenu(),
+              const _Settings(),
+              _Search(searchScreenProvider: searchScreenProvider),
+              _LightAndDarkTheme(themeProvider: themeProvider),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Search extends StatelessWidget {
+  const _Search({
+    super.key,
+    required this.searchScreenProvider,
+  });
+
+  final SearchScreenProvider searchScreenProvider;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 5),
+      child: ListTile(
+        leading: const Icon(Icons.search),
+        title: const Text('Buscar', style: TextStyle(fontSize: 20)),
+        onTap: () {
+          searchScreenProvider.cards = [];
+          searchScreenProvider.input = '';
+          Navigator.popAndPushNamed(context, 'search');
+        },
+      ),
+    );
+  }
+}
+
+class _Settings extends StatelessWidget {
+  const _Settings({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 5),
+      child: ListTile(
+        leading: const Icon(Icons.settings),
+        title: const Text('Configuración', style: TextStyle(fontSize: 20)),
+        onTap: () => Navigator.popAndPushNamed(context, 'settings'),
+      ),
+    );
+  }
+}
+
+class _MainMenu extends StatelessWidget {
+  const _MainMenu({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 5),
+      child: ListTile(
+        leading: const Icon(Icons.home),
+        title: const Text(
+          'Pantalla Principal',
+          style: TextStyle(fontSize: 20),
+        ),
+        onTap: () => Navigator.pop(context),
+      ),
+    );
+  }
+}
+
+class _Options extends StatelessWidget {
+  const _Options({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: const Text(
+        'Opciones',
+        style: TextStyle(fontSize: 20),
+      ),
+      trailing: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.close_sharp)),
+      //onTap: () => Navigator.pop(context),
+    );
+  }
+}
+
+class _LightAndDarkTheme extends StatefulWidget {
+  const _LightAndDarkTheme({super.key, required this.themeProvider});
+
+  final ThemeProvider themeProvider;
+
+  @override
+  State<_LightAndDarkTheme> createState() => _LightAndDarkThemeState();
+}
+
+class _LightAndDarkThemeState extends State<_LightAndDarkTheme> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 3),
+      child: ListTile(
+        leading: Preferences.isDarkMode
+            ? const Icon(Icons.dark_mode,
+                color: Color.fromARGB(255, 71, 144, 204), size: 30)
+            : const Icon(Icons.light_mode,
+                color: Color.fromARGB(255, 255, 196, 3), size: 30),
+        title: Preferences.isDarkMode
+            ? const Text('Modo Oscuro', style: TextStyle(fontSize: 20))
+            : const Text('Modo Claro', style: TextStyle(fontSize: 20)),
+        trailing: Switch.adaptive(
+          activeColor: Colors.grey,
+          value: Preferences.isDarkMode,
+          onChanged: (value) {
+            changeTheme(value, widget.themeProvider);
+            setState(() {});
+          },
         ),
       ),
     );
